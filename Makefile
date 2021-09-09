cd3dfdc2bcca83493fad56a1603fe16a05d16513
Antonio R. Collins II <ramar@collinsdesign.net>
2021-09-09 11:14:06 -0400

    got the hash algorithms working...

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CFLAGS=-Wall -Werror -Wno-unused
 
 # libgcrypt is part of GnuPG and handles different hashing functions
 main:
-	$(CC) $(CFLAGS) -lssl -lcrypto -o hash main.c
+	$(CC) $(CFLAGS) -o hash main.c libssl.a libcrypto.a
 
 # Neither of the crypto libs are working on Cygwin (wouldn't care, but this is what I'm stuck with for now)
 gnutls:
--- main.c
+++ main.c
@@ -5,12 +5,26 @@
 #include <unistd.h>
 #include <openssl/sha.h>
 
+
+struct algo_choice {
+	const char *alg;
+	int bufsize;
+	unsigned char *(*calc)( const unsigned char *, size_t, unsigned char *);	
+} algo_choices[] = {
+	{ "sha1"  , SHA_DIGEST_LENGTH, SHA1 }
+,	{ "sha224", SHA224_DIGEST_LENGTH, SHA224 }
+,	{ "sha256", SHA256_DIGEST_LENGTH, SHA256 }
+,	{ "sha384", SHA384_DIGEST_LENGTH, SHA384 }
+,	{ "sha512", SHA512_DIGEST_LENGTH, SHA512 }
+,	{ NULL }
+};
+
+
 int main ( int argc, char *argv[] ) {
 
 	unsigned char *arg = NULL;
 	char *algo = NULL;
-	int status = 0, arglen = 0;
-	SHA256_CTX c;
+	int status = 0, arglen = 0, v = 0;
 
 	if ( argc < 2 ) {
 		fprintf( stderr, "%s\n", "Usage: hash [ -a $ALGO ] <string>" );
@@ -18,12 +32,12 @@ int main ( int argc, char *argv[] ) {
 	}
 
 	while ( *argv ) {
-		if ( !strcmp( "-a", *argv ) || !strcmp( "--algorithm", *argv ) ) {
+		if ( !strcmp( "-a", *argv ) || !strcmp( "--algorithm", *argv ) )
 			++argv, algo = *argv;	
-		}	
-		else if ( !strcmp( "-f", *argv ) || !strcmp( "--file", *argv ) ) {
+		else if ( !strcmp( "-f", *argv ) || !strcmp( "--file", *argv ) )
 			++argv, arg = (unsigned char *)*argv;	
-		}	
+		else if ( !strcmp( "-v", *argv ) || !strcmp( "--verbose", *argv ) )
+			v = 1;	
 		else if ( **argv == '-' ) {
 			fprintf( stderr, "hash: Unknown argument '%s'\n", *argv );
 			return 1;
@@ -40,8 +54,36 @@ int main ( int argc, char *argv[] ) {
 		return 1;
 	}
 
-	unsigned char buf[ SHA512_DIGEST_LENGTH ];
-	memset( buf, 0, SHA512_DIGEST_LENGTH );	
-	unsigned char *sha = SHA256( arg, arglen, buf );
+	if ( !algo ) {
+		algo = "sha256";
+	}
+
+	for ( struct algo_choice *c = algo_choices; c->alg; c++ ) {
+		if ( !strcasecmp( c->alg, algo ) ) {
+			sel = c;
+			break;
+		}
+	}
+
+	if ( !sel ) {
+		fprintf( stderr, "hash: No algorithm '%s' available\n", algo );
+		return 1;
+	}
+
+	unsigned char *sha, *usr, buf[ sel->bufsize ], ret[ sel->bufsize * 2 ];
+	memset( buf, 0, sel->bufsize ), memset( ret, 0, sel->bufsize * 2 );	
+	sha = sel->calc( arg, arglen, buf ), usr = ret;
+
+	//Spits out a sequence of unsigned characters
+	//fprintf( stdout, "%s\n", sha );
+	
+	//We can do the following to show only printable characters
+	//THANKS to this StackOverflow post: 
+	//https://stackoverflow.com/questions/59112701/sha512-c-program-using-the-openssl-library 
+	for ( int i = 0; i < sel->bufsize; i++, sha++, usr += 2 ) {
+		sprintf( (char *)usr, "%02x", *sha );
+	}
+
+	printf( "%s\n", ret );
 	return 0;
 }
